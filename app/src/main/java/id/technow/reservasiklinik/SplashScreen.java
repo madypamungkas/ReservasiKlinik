6e7b64c3f2e34b47435cf8f93917c97cefe2dc13
Ady Pamungkas <madypamungkas@gmail.com>
2020-04-01 18:30:02 +0700

    first commit

---
+++ app/src/main/java/id/technow/reservasiklinik/SplashScreen.java
@@ -0,0 +1,16 @@
+package id.technow.reservasiklinik;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import id.technow.reservasiklinik.R;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
